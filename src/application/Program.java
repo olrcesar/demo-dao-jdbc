2d5f4a2210412d660deb2da5860b1fad14b10e0e
Ricardo Cesar <ol.rcesar@gmail.com>
2024-10-15 15:42:17 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,9 +31,15 @@ public class Program {
 		}
 		
 		System.out.println("\n===== TEST 4: seller insert =====");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n===== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha White");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 		
 	}
 }
